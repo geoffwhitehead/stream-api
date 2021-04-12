13bd6557b728585f5470c959722d1843f0efe725
Geoff Whitehead <geoff1012@gmail.com>
2021-04-12 18:15:29 +0100

    creates config file and refactors

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const config = {
+  port: process.env.port || 3000,
+  maxStreams: process.env.maxStreams || 3,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import express from "express";
+import { config } from "./config";
 import { routes } from "./routes";
-
-const PORT = process.env.port || 3000;
 
 const app = express();
 
 routes(app);
 
-app.listen(PORT, () => console.log("Listening on port", PORT));
+app.listen(config.port, () => console.log("Listening on port", config.port));
