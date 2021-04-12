e72be10a3f31bba4de8b78d6aa00d72c239a3c1b
Geoff Whitehead <geoff1012@gmail.com>
2021-04-12 20:07:52 +0100

    dockerify everything and update config

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,11 @@
-export const config = {
-  port: process.env.port || 3000,
-  maxStreams: process.env.maxStreams || 3,
+type Config = {
+  port: number;
+  maxStreams: number;
+  redisUrl: string;
+};
+
+export const config: Config = {
+  port: parseInt(process.env.PORT, 10) || 3000,
+  maxStreams: parseInt(process.env.MAX_STREAMS, 10) || 3,
+  redisUrl: process.env.REDIS_URL,
 };
