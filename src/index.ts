baa06552ddd4a39956883e582725b2e0c35da764
Geoff Whitehead <geoff1012@gmail.com>
2021-04-12 17:36:03 +0100

    creates simple read stream

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,45 @@
-import express from 'express'
+import express from "express";
+import fs from "fs";
 
-const PORT = 3000
+const PORT = 3000;
 
 const app = express();
+const SLOWDOWN = 1111;
 
-app.get('/', (req, res) => res.send('Hello'));
+app.get("/", (req, res) => res.sendFile(__dirname + "./src/index.html"));
 
-app.listen(PORT, () => console.log('Listening on port', PORT))
+app.get("/resource/:resourceId", (req, res) => {
+  const resourceMap: Record<string, string> = {
+    a: "mtb.mp4",
+  };
+
+  console.log(`req.headers`, req.headers);
+
+  const filePath =
+    __dirname + "/../assets/" + resourceMap[req.params.resourceId];
+
+  const stream = fs.createReadStream(filePath);
+
+  stream.on("error", (e: any) => {
+    if (e.code === "ENOENT") {
+      res.writeHead(404, "Not found");
+      res.end();
+    } else {
+      res.writeHead(500, "Server error");
+      res.end();
+    }
+  });
+
+  stream.on("data", (data) => {
+    res.write(data);
+  });
+
+  stream.on("end", () => {
+    console.log("end");
+    setTimeout(() => {
+      res.end();
+    }, SLOWDOWN);
+  });
+});
+
+app.listen(PORT, () => console.log("Listening on port", PORT));
