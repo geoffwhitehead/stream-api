1d67a035a613fd8f2c1c6c6119de5d49ff85d0a8
Geoff Whitehead <geoff1012@gmail.com>
2021-04-12 18:03:58 +0100

    assigns port to env

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { requestAssetStream } from "./services/asset";
 
-const PORT = 3000;
+const PORT = process.env.port || 3000;
 
 const app = express();
 const SLOWDOWN = 1111;
